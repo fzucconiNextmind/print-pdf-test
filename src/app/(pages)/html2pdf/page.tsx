6cb0c72e18f9b5c3c741f380bcdd9dec94ac231f
FrancescoZucconi <f.zucconi@nextmind.it>
2025-03-19 16:22:20 +0100

    Update header title and link text in PDF generator demo; change from jsPDF to Html2Pdf and update navigation link to Home

--- src/app/(pages)/html2pdf/page.tsx
+++ src/app/(pages)/html2pdf/page.tsx
@@ -100,11 +100,11 @@ export default function JsPdf() {
     <div className="sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-8">
         <h3 className="text-2xl font-bolt self-center">
-          Highcharts Pdf Generator Demo with: jsPDF
+          Highcharts Pdf Generator Demo with: Html2Pdf
         </h3>
         <div className="flex justify-between">
           <Link className="self-start" href={"/"}>
-            try with react-to-pdf
+            Home
           </Link>
           <button
             onClick={() => convertToPdf("test")}
