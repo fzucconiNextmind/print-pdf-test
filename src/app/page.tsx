28172e9a27b74a48ee20c2b05339f7c6b0aa08c4
FrancescoZucconi <f.zucconi@nextmind.it>
2025-03-21 17:29:28 +0100

    Refactor Home page to include HeaderComponent for improved layout; update header title for clarity and enhance styling with shadow effects.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
+import HeaderComponent from "@/components/pdfPreview.tsx/HeaderComponent";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center ">
-        <h3 className="text-2xl font-bolt self-center">
-          Highcharts Pdf Generator Demo with: @react-to-pdf
-        </h3>
+        <HeaderComponent />
         <Link className="self-start" href={"/react-to-pdf"}>
           try with @react-to-pdf
         </Link>
--- src/components/pdfPreview.tsx/HeaderComponent.tsx
+++ src/components/pdfPreview.tsx/HeaderComponent.tsx
@@ -2,10 +2,10 @@ const HeaderComponent = () => {
   return (
     <div
       id="pdf-header"
-      className="flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear tracking-wide"
+      className="flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear tracking-wide shadow-[0_24px_32px_-10px_rgba(0,255,0,0.4),0_-10px_0px_0px_rgba(0,255,0,0)]"
     >
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <h1 className="text-base font-medium text-blue-500">NextMind</h1>
+        <h1 className="text-base font-medium">Highcharts Pdf Generator Demo</h1>
       </div>
     </div>
   );
